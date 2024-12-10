db59c2227f75e98ad8904bb7bf08481511add531
Thomas Braun <tbraun96@gmail.com>
2024-12-10 12:51:42 -0500

    fix: cargo test passes

--- tests/bls.rs
+++ tests/bls.rs
@@ -1,14 +1,8 @@
-use std::sync::atomic::AtomicU64;
-
 use bls_blueprint::keygen::KEYGEN_JOB_ID;
 use bls_blueprint::signing::SIGN_JOB_ID;
 
 const N: usize = 3;
 const T: usize = 2;
-
-// The macro takes this variable as an argument, and will update it so that
-// when we pass the signing arguments, we can pass the associated keygen call id
-static KEYGEN_CALL_ID: AtomicU64 = AtomicU64::new(0);
 
 use blueprint_test_utils::tangle::NodeConfig;
 use blueprint_test_utils::test_ext::new_test_ext_blueprint_manager;
@@ -20,10 +14,10 @@ use blueprint_test_utils::{
 #[tokio::test(flavor = "multi_thread")]
 async fn test_blueprint() {
     setup_log();
-
+    gadget_sdk::info!("Running BLS blueprint test");
     let tmp_dir = blueprint_test_utils::tempfile::TempDir::new().unwrap();
     let tmp_dir_path = tmp_dir.path().to_string_lossy().into_owned();
-    let node_config = NodeConfig::new(false);
+    let node_config = NodeConfig::new(true);
     new_test_ext_blueprint_manager::<N, 1, String, _, _>(
         tmp_dir_path,
         run_test_blueprint_manager,
@@ -36,8 +30,7 @@ async fn test_blueprint() {
 
         let service_id = service.id;
         gadget_sdk::info!(
-            "Submitting KEYGEN job {} with service ID {service_id}",
-            KEYGEN_JOB_ID
+            "Submitting KEYGEN job {KEYGEN_JOB_ID} with service ID {service_id}",
         );
 
         let job_args = vec![(InputValue::Uint16(T as u16))];
@@ -49,7 +42,7 @@ async fn test_blueprint() {
             client,
             &keypair,
             service_id,
-            Job::from(0),
+            Job::from(KEYGEN_JOB_ID),
             job_args,
             call_id,
         )
@@ -57,7 +50,6 @@ async fn test_blueprint() {
         .expect("Failed to submit job");
 
         let keygen_call_id = job.call_id;
-        KEYGEN_CALL_ID.store(keygen_call_id, std::sync::atomic::Ordering::Relaxed);
 
         gadget_sdk::info!(
             "Submitted KEYGEN job {} with service ID {service_id} has call id {keygen_call_id}",
@@ -93,7 +85,6 @@ async fn test_blueprint() {
         gadget_sdk::info!("Keygen job completed successfully! Moving on to signing ...");
 
         let service = &blueprint.services[0];
-
         let service_id = service.id;
         gadget_sdk::info!(
             "Submitting SIGNING job {} with service ID {service_id}",
@@ -101,7 +92,7 @@ async fn test_blueprint() {
         );
 
         let job_args = vec![
-            InputValue::Uint64(KEYGEN_CALL_ID.load(std::sync::atomic::Ordering::SeqCst)),
+            InputValue::Uint64(keygen_call_id),
             InputValue::List(BoundedVec(vec![
                 InputValue::Uint8(1),
                 InputValue::Uint8(2),
@@ -113,7 +104,7 @@ async fn test_blueprint() {
             client,
             &keypair,
             service_id,
-            Job::from(1),
+            Job::from(SIGN_JOB_ID),
             job_args,
             call_id + 1,
         )
@@ -121,10 +112,8 @@ async fn test_blueprint() {
         .expect("Failed to submit job");
 
         let signing_call_id = job.call_id;
-
         gadget_sdk::info!(
-            "Submitted SIGNING job {} with service ID {service_id} has call id {signing_call_id}",
-            SIGN_JOB_ID
+            "Submitted SIGNING job {SIGN_JOB_ID} with service ID {service_id} has call id {signing_call_id}",
         );
 
         let job_results = wait_for_completion_of_tangle_job(client, service_id, signing_call_id, T)
